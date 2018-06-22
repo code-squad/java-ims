5d14a1a8faf51f1458c995a6b305f88c81717fee
larryjung <corbswjd@gmail.com>
2018-06-22 16:09:32 +0900

    Chanege label controller to restcontroller

--- src/main/java/codesquad/web/LabelController.java
+++ src/main/java/codesquad/web/LabelController.java
@@ -5,24 +5,22 @@ import codesquad.domain.Label;
 import codesquad.service.IssueService;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Collection;
 
-@Controller
-@RequestMapping("/labels")
+@RestController
+@RequestMapping("/api/labels")
 public class LabelController {
 
     @Resource(name = "issueService")
     private IssueService issueService;
 
-    @GetMapping("") // 라벨끼리 묶는 것은 했는데..
-    public @ResponseBody ImmutableMap<Label, Collection<Issue>> listPage(Model model) {
+    @GetMapping("")
+    public ImmutableMap<Label, Collection<Issue>> listPage() {
         ImmutableListMultimap<Label, Issue> labelOrderedIssues = issueService.findByLabel();
         return labelOrderedIssues.asMap();
     }
