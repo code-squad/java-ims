68ebd28108c0e5f97ec5843821fab64f9eb90d8c
larryjung <corbswjd@gmail.com>
2018-06-26 11:30:12 +0900

    Write test titles for answer

---
+++ src/test/java/codesquad/web/ApiAnswerAcceptanceTest.java
@@ -0,0 +1,47 @@
+package codesquad.web;
+
+import org.junit.Test;
+import support.test.AcceptanceTest;
+
+public class ApiAnswerAcceptanceTest extends AcceptanceTest{
+
+    @Test
+    public void create_success() {
+
+    }
+
+    @Test
+    public void create_fail_no_login() {
+
+    }
+
+    @Test
+    public void update_success() {
+
+    }
+
+    @Test
+    public void update_fail_no_login() {
+
+    }
+
+    @Test
+    public void update_fail_other() {
+
+    }
+
+    @Test
+    public void delete_success() {
+
+    }
+
+    @Test
+    public void delete_fail_no_login() {
+
+    }
+
+    @Test
+    public void delete_fail_other() {
+
+    }
+}
