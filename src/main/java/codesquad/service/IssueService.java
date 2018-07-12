9ff848dfa9266e8132c9ad0d5a582c79de1b646a
HYEON SEONG CHUN <godlovesjoe@naver.com>
2018-07-12 15:01:38 +0900

    issueService추가

--- src/main/java/codesquad/service/IssueService.java
+++ src/main/java/codesquad/service/IssueService.java
@@ -1,6 +1,8 @@
 package codesquad.service;
 
+import codesquad.domain.Issue;
 import codesquad.domain.IssueRepository;
+import codesquad.dto.IssueDto;
 
 import javax.annotation.Resource;
 
@@ -8,5 +10,13 @@ public class IssueService {
     @Resource(name = "issueRepository")
     private IssueRepository issueRepository;
 
+    public Issue add(IssueDto issueDto) {
+        return issueRepository.save(issueDto.toIssue());
+    }
+
+    public Issue findById(long id) {
+        return issueRepository.findById(id)
+                .orElseThrow(NullPointerException::new);
+    }
 
 }
--- src/main/java/codesquad/web/IssueController.java
+++ src/main/java/codesquad/web/IssueController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -20,10 +21,8 @@ public class IssueController {
     private IssueService issueService;
 
     @GetMapping("form")
-    public String form(@LoginUser User loginUser) {
+    public String form() {
         return "/issue/form";
     }
-
-
 
 }
