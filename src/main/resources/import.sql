0ae83f75af5ba2a0cdb0c8ef6c74dfa51224927f
David <xodnjs2549@naver.com>
2018-07-12 16:46:52 +0900

    refactor : 사용자 정보 업데이트 수정

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,2 @@
-INSERT INTO user (id, user_id, password, name) values (1, 'javajigi', 'password', '자바지기');
-INSERT INTO user (id, user_id, password, name) values (2, 'sanjigi', 'password', '산지기');
-INSERT INTO user (id, user_id, password, name) values (3, 'learner', 'password', '러너네임');
-
-INSERT INTO MILESTONE (subject, end_date) values ('MILESTONE 01', '2018-08-01 17:55:00');
-
-INSERT INTO ISSUE (contents, deleted, subject, milestone_id, writer_id) values ('불일치 이슈', false, '불일치 이슈 제목', 1, 3);
-
-INSERT INTO LABEL (TITLE) VALUES ('DB');
-INSERT INTO LABEL (TITLE) VALUES ('NOTICE');
-
+INSERT INTO user (id, user_id, password, name) values (1, 'javajigi', 'testtest', '자바지기');
+INSERT INTO user (id, user_id, password, name) values (2, 'sanjigi', 'testtest', '산지기');
