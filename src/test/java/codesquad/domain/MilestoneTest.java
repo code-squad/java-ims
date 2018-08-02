01858aa9e55647337f8b6683007c61a2a2d8b83a
David <xodnjs2549@naver.com>
2018-08-02 22:59:49 +0900

    refactor : milestoneTest 추가

--- src/test/java/codesquad/domain/MilestoneTest.java
+++ src/test/java/codesquad/domain/MilestoneTest.java
@@ -1,41 +1,4 @@
 package codesquad.domain;
 
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 public class MilestoneTest {
-    private static final Logger log = LoggerFactory.getLogger(MilestoneTest.class);
-
-    @Test
-    public void create() {
-        Milestone milestone = new Milestone();
-    }
-
-    // datetime-local은 html5 date picker의 이름
-    @Test
-    public void convert_datetime_local_to_localdatetime() {
-        String endDate = "2017-06-01T08:30";
-        Milestone milestone = new Milestone();
-        milestone.setHtml_enddate(endDate);
-
-        LocalDateTime convertedDate = LocalDateTime.parse(endDate, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
-
-        assertThat(milestone.getEndDate(),is(convertedDate));
-    }
-
-    @Test
-    public void getDueDate() {
-        String date = "2017-06-01T08:30";
-        LocalDateTime endDate = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
-        String result = endDate.format(DateTimeFormatter.ofPattern("'Due by' MMM dd, yyyy"));
-
-        System.out.println(result);
-    }
 }
--- src/test/java/codesquad/web/MilestoneAcceptanceTest.java
+++ src/test/java/codesquad/web/MilestoneAcceptanceTest.java
@@ -1,20 +1,43 @@
 package codesquad.web;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
+import support.test.HtmlFormDataBuilder;
+
+import java.util.HashMap;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MilestoneAcceptanceTest extends AcceptanceTest {
+    private static final Logger log =  LoggerFactory.getLogger(MilestoneAcceptanceTest.class);
 
     @Test
     public void list() {
-        TestRestTemplate template = basicAuthTemplate(findDefaultUser());
         ResponseEntity<String> response = template.getForEntity("/milestones", String.class);
+        assertThat(response.getStatusCode(),is(HttpStatus.OK));
+    }
+
+    @Test
+    public void createForm() {
+        TestRestTemplate template = basicAuthTemplate(findDefaultUser());
+        ResponseEntity<String> response = template.getForEntity("/milestones/form", String.class);
+        assertThat(response.getStatusCode(),is(HttpStatus.OK));
+    }
+
+    public void create() {
+        TestRestTemplate template = basicAuthTemplate(findDefaultUser());
+        HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm()
+                .addParameter("subject", "생성된 첫 번째 이슈 제목")
+                .addParameter("comment", "생성된 첫 번째 이슈 내용").build();
+        ResponseEntity<String> response = template.postForEntity("/milestones", request, String.class);
         assertThat(response.getStatusCode(),is(HttpStatus.OK));
     }
 }
