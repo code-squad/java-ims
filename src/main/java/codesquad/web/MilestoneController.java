8baeab012841228ca3d4f8e4898905243586aaeb
David <xodnjs2549@naver.com>
2018-08-02 22:59:49 +0900

    refactor : milestoneTest 추가

--- src/main/java/codesquad/web/MilestoneController.java
+++ src/main/java/codesquad/web/MilestoneController.java
@@ -1,6 +1,10 @@
 package codesquad.web;
 
+import codesquad.domain.User;
+import codesquad.security.HttpSessionUtils;
+import codesquad.security.LoginUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,4 +17,8 @@ public class MilestoneController {
         return "/milestone/list";
     }
 
+    @GetMapping("/form")
+    public String createForm(@LoginUser User user) {
+        return "/milestone/form";
+    }
 }
