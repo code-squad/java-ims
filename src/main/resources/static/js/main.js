7ea08d767916b863e05e412c1941adf7139f125f
David <xodnjs2549@naver.com>
2018-09-06 15:12:59 +0900

    refactor : 수정버튼에 업데이트 요청 url 삽입

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,7 +2,9 @@
 // $(document).on("click", "set_milestone_submit", setMilestone);
 $("#addCommentSubmit").on("click", addComment);
 // $(".buttons .update_comment").on("click", updateComment);
-$(document).on("click", ".buttons .update_comment", updateComment);
+// $(document).on("click", ".buttons .update_comment", updateFormSetting);
+// $(document).on("click", ".buttons .update_comment", {value:$(".comment_area .id").attr("value")}, updateFormSetting);
+$(document).on("click", ".buttons .update_comment", updateFormSetting);
 
 function assign(e) {
     e.preventDefault();
@@ -39,16 +41,40 @@ function addComment(e) {
             console.log(status);
             var commentTemplate = $("#commentTemplate").html();
             console.log(commentTemplate);
-            var template = commentTemplate.format(data.writer.userId, data.contents);
+            var template = commentTemplate.format(data.writer.userId, data.contents, data.id);
             $("#comment-bottom").before(template);
             $("#contents").val("");
         }
     });
 }
 
-function updateComment(e) {
+function updateFormSetting(e) {
+    e.preventDefault();
     var comment_contents = $(e.target).closest(".comment_area").find(".contents").text();
     console.log("contents : " + comment_contents);
+
+    var id = $(e.target).closest(".comment_area").find(".id").attr("value");
+    console.log("commentId : " + id);
+
+    $("#addCommentSubmit").toggle();
+    $("#updateCommentSubmit").toggle();
+
+    var path = window.location.pathname; // /issue/1
+    var index = path.lastIndexOf('/');
+    var issueId = path.substring(index+1);
+    console.log("issueId : " + issueId);
+    var updateRequestUrl = "/api/issues/"+ issueId +"/comments/"+id;
+    console.log("update request url : " + updateRequestUrl);
+    $("#updateCommentSubmit").attr("data-message", updateRequestUrl);
+
+    //동적으로 코멘트를 추가하고 수정 버튼을 눌렀을 떄, textarea에 수정할 코멘트 내용을 세팅하기 위해 .text를 사용하면 세팅이 안 된다.
+    // val을 사용했을 때에는 된다.
+    // $("#contents").text(comment_contents);
+    $("#contents").val(comment_contents);
+}
+
+function updateComment(e) {
+
 }
 
 function onError() {
