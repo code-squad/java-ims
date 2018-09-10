124e0800d14b13d7a151e6a8e7798ca2554c6769
David <xodnjs2549@naver.com>
2018-09-10 17:42:15 +0900

    refactor : conflict fix

--- src/test/java/codesquad/domain/IssueTest.java
+++ src/test/java/codesquad/domain/IssueTest.java
@@ -72,40 +72,4 @@ public class IssueTest {
 
         assertThat(comments.size()>0, is(true));
     }
-
-    @Test
-    public void matchWriter() {
-        User writer = new User(1L, "learner", "test1234", "taewon");
-        Issue issue = new Issue("사용자 일치 이슈", "코멘트 내용", writer);
-
-        assertThat(issue.matchWriter(writer), is(true));
-    }
-
-    @Test
-    public void setMilestone() {
-        Milestone milestone = new Milestone("사용자 일치 기능");
-        Issue issue = new Issue("사용자 일치 이슈", "코멘트 내용");
-
-        issue.registerMilestone(milestone);
-        assertThat(issue.toString().contains("사용자 일치 기능"), is(true));
-    }
-
-    @Test
-    public void setAssignee() {
-        User writer = new User(3L, "learner", "password", "taewon");
-        Issue issue = new Issue(4L, "사용자 일치 이슈", "이슈 내용", writer);
-
-        issue.registerAssignee(writer);
-        System.out.println(issue.toString());
-        assertThat(issue.toString().contains("3"), is(true));
-    }
-
-    @Test
-    public void setLabel() {
-        Label label = new Label("라벨");
-        Issue issue = new Issue("라벨 일치 이슈", "코멘트 내용");
-
-        issue.registerLabel(label);
-        assertThat(issue.toString().contains("라벨"),is(true));
-    }
 }
