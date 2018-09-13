0509d87fe3d2d005ecf84028f070b8b112f9e3c4
David <xodnjs2549@naver.com>
2018-09-13 13:26:34 +0900

    feat : 폼으로 부터 넘어온 파일 저장 기능 추가

--- src/main/java/codesquad/domain/FileInfo.java
+++ src/main/java/codesquad/domain/FileInfo.java
@@ -1,14 +1,28 @@
 package codesquad.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Entity
 public class FileInfo {
+    private static final Logger log =  LoggerFactory.getLogger(FileInfo.class);
+
+    @Transient
     public static final Path rootLocation = Paths.get("target/files/");
 
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 20, nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    @Convert(converter = PathConverter.class)
     private Path path; // target/files/sample.txt
 
     public FileInfo() {
@@ -23,9 +37,17 @@ public class FileInfo {
         this.path = path;
     }
 
-    public FileInfo(MultipartFile file) {
-        this.name = file.getName();
-        this.path = rootLocation.resolve(file.getOriginalFilename());
+    public FileInfo(MultipartFile file, Path dirPath) {
+        this.name = file.getName(); // file
+
+        int index = file.getOriginalFilename().lastIndexOf("\\"); // C:/image/img001.jpg
+        String originalName = file.getOriginalFilename().substring(index+1); // img001.jpg
+        this.path = rootLocation.resolve(dirPath).resolve(originalName); // target/files/3456789123/img001.jpg
+
+        log.debug("rootLocation : {}", rootLocation.toString());
+        log.debug("index : {}", index);
+        log.debug("original name : {}", originalName);
+        log.debug("this path : {}", this.path);
     }
 
     public String getName() {
@@ -42,5 +64,13 @@ public class FileInfo {
 
     public void setPath(Path path) {
         this.path = path;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/main/java/codesquad/service/FileStorageService.java
+++ src/main/java/codesquad/service/FileStorageService.java
@@ -1,35 +1,50 @@
 package codesquad.service;
 
 import codesquad.domain.FileInfo;
+import codesquad.domain.FileStorageRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
+import java.security.Permission;
 import java.util.Random;
 
 @Service
 public class FileStorageService {
     private static final Logger log =  LoggerFactory.getLogger(FileStorageService.class);
 
-    public FileInfo storeFile(MultipartFile file) {
+    @Resource(name = "fileStorageRepository")
+    private FileStorageRepository fileStorageRepository;
 
-        FileInfo fileInfo = new FileInfo(file);
-        Path location = FileInfo.rootLocation.resolve(String.valueOf(Math.abs(new Random().nextLong())));
+    public FileInfo storeFile(MultipartFile file) {
+        Path dirPath = Paths.get(String.valueOf(Math.abs(new Random().nextLong())));
+        FileInfo fileInfo = new FileInfo(file, dirPath);
         try {
-            Files.createDirectories(location);
+            Path resultPath = Files.createDirectories(dirPath);
+            Path fullDirPath = FileInfo.rootLocation.resolve(resultPath);
+            fullDirPath = Files.createDirectory(fullDirPath);
+            File dir = new File(fullDirPath.toString());
+            dir.setWritable(true);
+            log.debug("fullDirPath : {}", fullDirPath.toString());
+            log.debug("result path : {}", resultPath.toString());
+
+            log.debug("fileInfo Path : {}", fileInfo.getPath());
             InputStream inputStream = file.getInputStream();
-            Files.copy(inputStream, location.resolve(file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(inputStream, fileInfo.getPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return fileInfo;
+    }
+
+    public FileInfo storeFileInfo(FileInfo fileInfo) {
+        return fileStorageRepository.save(fileInfo);
     }
 }
