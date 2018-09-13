0be174ddbff44cb51bda4b6f43964779dfd220ca
David <xodnjs2549@naver.com>
2018-09-13 14:48:19 +0900

    refactor : 잘못 생성된 폴더 업로드 폴더 삭제

--- src/test/java/codesquad/domain/DirectoryPathMakerTest.java
+++ src/test/java/codesquad/domain/DirectoryPathMakerTest.java
@@ -1,13 +1,22 @@
 package codesquad.domain;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class DirectoryPathMakerTest {
+    private static final Logger log =  LoggerFactory.getLogger(DirectoryPathMakerTest.class);
+
     @Test
     public void makePath() {
         DirectoryPathMaker pathMaker = new DirectoryPathMaker();
         Path dirPath = pathMaker.makePath();
+        log.debug("dirPath : {}", dirPath);
+        assertThat(dirPath.toString().contains("target\\files\\"), is(true));
     }
 }
