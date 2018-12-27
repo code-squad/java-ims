d77a9c91ad763ba242c6db70dc9659201b4b65e6
Brad Lee <brad903@naver.com>
2018-12-27 23:49:05 +0900

    Issue Entity generated

---
+++ src/main/java/codesquad/domain/Issue.java
@@ -0,0 +1,51 @@
+package codesquad.domain;
+
+import support.domain.AbstractEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Issue extends AbstractEntity {
+
+    @Size(min = 3, max = 100)
+    @Column(length = 100, nullable = false)
+    private String subject;
+
+    @Size(min = 5)
+    @Lob
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_issue_writer"))
+    private User writer;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public User getWriter() {
+        return writer;
+    }
+
+    public void setWriter(User writer) {
+        this.writer = writer;
+    }
+
+    @Override
+    public String toString() {
+        return "Issue [subject=" + subject + ", comment=" + comment + ", writer=" + writer + ']';
+    }
+}
