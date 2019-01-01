d5ddfd1fbe5895dca83b15a1f2416b0f1226604c
Brad Lee <brad903@naver.com>
2019-01-01 10:55:51 +0900

    MilestoneController 구현 및 HTML 매핑

---
+++ src/main/java/codesquad/web/MilestoneController.java
@@ -0,0 +1,22 @@
+package codesquad.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/milestones")
+public class MilestoneController {
+
+    @GetMapping("")
+    public String list() {
+        return "milestone/list";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "milestone/form";
+    }
+
+
+}
