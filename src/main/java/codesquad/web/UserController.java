1833a44b4e0de7a10d2b6cbc73fcf1a29377cec2
tskim <lollolzkk@gmail.com>
2019-01-02 17:07:23 +0900

    step1 ver_01 complete (make user_login fuction)

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -1,7 +1,9 @@
 package codesquad.web;
 
+import codesquad.UnAuthenticationException;
 import codesquad.domain.User;
 import codesquad.dto.UserDto;
+import codesquad.security.HttpSessionUtils;
 import codesquad.security.LoginUser;
 import codesquad.service.UserService;
 import org.slf4j.Logger;
@@ -11,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -29,6 +33,27 @@ public class UserController {
     public String create(UserDto userDto) {
         userService.add(userDto);
         return "redirect:/users";
+    }
+
+    @GetMapping("")
+    public String list() {
+        return "/index";
+    }
+
+    @GetMapping("/login/form")
+    public String loginForm(){
+        return "/user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession session) {
+        try {
+            User user = userService.login(userId, password);
+            session.setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
+            return "redirect:/users";
+        } catch (UnAuthenticationException e) {
+            return "user/login_failed";
+        }
     }
 
     @GetMapping("/{id}/form")
