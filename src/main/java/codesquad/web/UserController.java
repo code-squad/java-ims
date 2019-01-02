dd77dc8207834dd9b6dd714a0a61f4901abb7aa5
foreverever <lollolzkk@gmail.com>
2019-01-02 20:21:42 +0900

    java-ims Step1 (#115)
    * step1 ver_01 complete (make user_login fuction)
    
    * step1 ver_02 complete (make user_update fuction)
    
    * step1 ver_03 complete (deduplication html code by making base.html)
    
    * step1 ver_04 complete (refactoring)

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -1,7 +1,9 @@
 package codesquad.web;
 
+import codesquad.UnAuthenticationException;
 import codesquad.domain.User;
 import codesquad.dto.UserDto;
+import codesquad.security.HttpSessionUtils;
 import codesquad.security.LoginUser;
 import codesquad.service.UserService;
 import org.slf4j.Logger;
@@ -11,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -31,6 +35,27 @@ public class UserController {
         return "redirect:/users";
     }
 
+    @GetMapping("")
+    public String list() {
+        return "/index";
+    }
+
+    @GetMapping("/login/form")
+    public String loginForm(){
+        return "/user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession session) {
+        try {
+            User user = userService.login(userId, password);
+            session.setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
+            return "redirect:/users";
+        } catch (UnAuthenticationException e) {
+            return "user/login_failed";
+        }
+    }
+
     @GetMapping("/{id}/form")
     public String updateForm(@LoginUser User loginUser, @PathVariable long id, Model model) {
         log.debug("LoginUser : {}", loginUser);
@@ -43,5 +68,4 @@ public class UserController {
         userService.update(loginUser, id, target);
         return "redirect:/users";
     }
-
 }
