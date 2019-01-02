26aae5b4f4a804c81598bf8aef0a78951ac21b46
geonHee <corqkrtk3@naver.com>
2019-01-02 18:08:58 +0900

    step1 web connect

--- src/main/java/codesquad/security/SecurityControllerAdvice.java
+++ src/main/java/codesquad/security/SecurityControllerAdvice.java
@@ -5,13 +5,14 @@ import codesquad.UnAuthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.EntityNotFoundException;
 
-@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
 public class SecurityControllerAdvice {
     private static final Logger log = LoggerFactory.getLogger(SecurityControllerAdvice.class);
 
