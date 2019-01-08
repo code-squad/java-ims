b1ac2a794e779a0392020785de3a8aab830207b9
tskim <lollolzkk@gmail.com>
2019-01-08 22:31:57 +0900

    step4 ver_10 complete (domain test about Milestone,Label)

---
+++ src/test/java/codesquad/domain/MilestoneTest.java
@@ -0,0 +1,19 @@
+package codesquad.domain;
+
+import support.test.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MilestoneTest extends BaseTest {
+    public static final Milestone milestone1 = new Milestone(1L, "milestone1", "2019-01-05", "2019-01-09");
+    public static final Milestone milestone2 = new Milestone(2L, "milestone2", "2019-01-06", "2019-01-12");
+    public static final Milestone milestone3 = new Milestone(3L, "milestone3", "2019-01-07", "2019-01-10");
+
+    public static final List<Milestone> milestones = new ArrayList<>();
+    static {
+        milestones.add(milestone1);
+        milestones.add(milestone2);
+        milestones.add(milestone3);
+    }
+}
