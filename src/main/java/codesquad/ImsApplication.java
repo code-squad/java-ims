dc6c7d5be2356b0cbf39cb6c0d9e9a3b9bfa0569
zingoworks <zingoworks@gmail.com>
2019-01-23 12:29:33 +0900

    Init step6

--- src/main/java/codesquad/ImsApplication.java
+++ src/main/java/codesquad/ImsApplication.java
@@ -15,7 +15,7 @@ public class ImsApplication {
 
 
 /**
- * Todo : Step5 ~ing
+ * Todo : Step6 ~ing 파일첨부하기
  * 1. html 자원들 중복 제거하면서 스타일이 제대로 안나오는 부분이 있다, 추후 중복 제거 다시해서 레이아웃이 제대로 나오도록
  * 2. 회원가입/가입정보수정 시 유효하지 않은 값 입력하고 submit 시 어떤 경우에 alert, 어떤 경우에 오류페이지 나오는지 확인 후 개선
  */
